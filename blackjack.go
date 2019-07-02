8c194fdfdc0955d9500febc81d53e656975c755f
depretiger <depretiger@gmail.com>
2019-07-03 00:53:14 +0900

    とりあえず完成

--- blackjack.go
+++ blackjack.go
@@ -5,24 +5,14 @@ import (
 	"math/rand"
 	"time"
 )
-func judge(sumD,sumP,dealer,player){
-    if sumD >= 22 && sumP >=22 {
-        fmt.Printf("This game is draw.\n")
-    }
-    if sumD >= 22 && sumP <= 21 {
-        fmt.Printf("Dealer is burst!\nYou win!\n")
-    }
-    if sumD <= 21 && sumP >= 22 {
-        fmt.Printf("Player is burst!\nYou lose.\n")
-    }
-    if sumD <= 21 && sumP <= 21 {
-        if sumD >sumP {
-            fmt.Printf("You lose.")
-        }
-        if sumD < sumP {
-            fmt.Printf("You win!")
-        }
-    }
+
+func judge(sumD int, sumP int) {
+	if sumD > sumP {
+		fmt.Printf("You lose.")
+	}
+	if sumD < sumP {
+		fmt.Printf("You win!")
+	}
 }
 
 func drawCard(hand *[]int, sum *int) {
@@ -51,18 +41,27 @@ func main() {
 	for status != "Stand" {
 		fmt.Println("Hit or Stand?")
 		fmt.Scanf("%s", &status)
-		drawCard(&player, &sumP)
-		fmt.Println("player: ", player, "合計:", sumP)
-		if sumP > 21 {
-			fmt.Println("You Lose")
-			return
+		if status == "Hit" {
+			drawCard(&player, &sumP)
+			fmt.Println("player: ", player, "合計:", sumP)
+			if sumP > 21 {
+				fmt.Println("You Lose")
+				return
+			}
+
 		}
 	}
 	//ディーラーは17以上になるまで引き続ける
 	for sumD <= 16 {
 		drawCard(&dealer, &sumD)
+		if sumD > 21 {
+			fmt.Println("You win")
+			return
+		}
+
 	}
 	fmt.Println("dealer: ", dealer, "合計:", sumD)
 	fmt.Println("player: ", player, "合計:", sumP)
 
+	judge(sumD, sumP)
 }
