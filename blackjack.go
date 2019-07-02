c60833b314f97c89606d4a8c65db54a6c9b6c4a7
pingoon <kime.z.c7@gmail.com>
2019-07-02 15:31:45 +0000

    judge

--- blackjack.go
+++ blackjack.go
@@ -3,6 +3,25 @@ package main
 import (
 	"fmt"
 )
+func judge(sumD,sumP,dealer,player){
+    if sumD >= 22 && sumP >=22 {
+        fmt.Printf("This game is draw.\n")
+    }
+    if sumD >= 22 && sumP <= 21 {
+        fmt.Printf("Dealer is burst!\nYou win!\n")
+    }
+    if sumD <= 21 && sumP >= 22 {
+        fmt.Printf("Player is burst!\nYou lose.\n")
+    }
+    if sumD <= 21 && sumP <= 21 {
+        if sumD >sumP {
+            fmt.Printf("You lose.")
+        }
+        if sumD < sumP {
+            fmt.Printf("You win!")
+        }
+    }
+}
 
 func drawCard(hand [10]int)
 
