170b00fe7b10b3c8f04c5bea87475e900da51fc7
depretiger <depretiger@gmail.com>
2019-07-02 23:46:21 +0900

    スライスを作った

--- blackjack.go
+++ blackjack.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 )
 
+func drawCard(hand [10]int)
+
 func main() {
-	var dealer [10]int
-	var player [10]int
+	var dealer []int
+	var player []int
+	dealer = make([]int, 0, 10)
+	player = make([]int, 0, 10)
 	sumD := 0
 	sumP := 0
 	fmt.Println(dealer[1])
