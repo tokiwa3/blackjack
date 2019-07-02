3e11f96443d1eb52015ed70e335e06fe57e1ea85
depretiger <depretiger@gmail.com>
2019-07-03 00:38:36 +0900

    引くとこ作った

--- blackjack.go
+++ blackjack.go
@@ -2,20 +2,48 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-func drawCard(hand [10]int)
+func drawCard(hand *[]int, sum *int) {
+	rand.Seed(time.Now().UnixNano())
+
+	newCard := rand.Intn(10) + 1
+	*hand = append(*hand, newCard)
+	*sum += newCard
+}
 
 func main() {
 	var dealer []int
 	var player []int
-	dealer = make([]int, 0, 10)
-	player = make([]int, 0, 10)
 	sumD := 0
 	sumP := 0
-	fmt.Println(dealer[1])
-	fmt.Println(player)
-	fmt.Printf("%T", sumD)
-	fmt.Printf("%T", sumP)
+	dealer = make([]int, 0, 10)
+	player = make([]int, 0, 10)
+	for i := 0; i < 2; i++ {
+		drawCard(&dealer, &sumD)
+		drawCard(&player, &sumP)
+	}
+	fmt.Println("dealer: ", dealer[0])
+	fmt.Println("player: ", player, "合計:", sumP)
+
+	status := ""
+	for status != "Stand" {
+		fmt.Println("Hit or Stand?")
+		fmt.Scanf("%s", &status)
+		drawCard(&player, &sumP)
+		fmt.Println("player: ", player, "合計:", sumP)
+		if sumP > 21 {
+			fmt.Println("You Lose")
+			return
+		}
+	}
+	//ディーラーは17以上になるまで引き続ける
+	for sumD <= 16 {
+		drawCard(&dealer, &sumD)
+	}
+	fmt.Println("dealer: ", dealer, "合計:", sumD)
+	fmt.Println("player: ", player, "合計:", sumP)
 
 }
